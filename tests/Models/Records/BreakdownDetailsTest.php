c6c1bd0a2f77de922ca48226a084327495f9b8ef
José Miguel Moreno <josemmo@pm.me>
2025-11-08 14:24:42 +0100

    Improved tests for surchage rate and amount
    - Updated unit tests

--- tests/Models/Records/BreakdownDetailsTest.php
+++ tests/Models/Records/BreakdownDetailsTest.php
@@ -12,44 +12,72 @@ final class BreakdownDetailsTest extends TestCase {
     public function testValidatesTaxAmount(): void {
         $details = new BreakdownDetails();
         $details->taxType = TaxType::IVA;
-        $details->regimeType = RegimeType::C18;
+        $details->regimeType = RegimeType::C01;
         $details->operationType = OperationType::Subject;
         $details->baseAmount = '11.22';
-        $details->taxRate = '21.00';
-        $details->taxAmount = '2.36';
-        $details->surchargeRate = '5.20';
-        $details->surchargeAmount = '0.58';
+        $details->taxRate = '10.00';
+        $details->taxAmount = '1.12';
 
         // Should pass validation
         $details->validate();
 
         // Wrong tax amount
         $details->taxAmount = '99.99';
-        $details->surchargeAmount = '99.99';
         try {
             $details->validate();
             $this->fail('Did not throw exception for invalid tax amount');
         } catch (InvalidModelException $e) {
-            $this->assertStringContainsString('Expected tax amount of 2.36, got 99.99', $e->getMessage());
-            $this->assertStringContainsString('Expected surcharge amount of 0.58, got 99.99', $e->getMessage());
+            $this->assertStringContainsString('Expected tax amount of 1.12, got 99.99', $e->getMessage());
         }
 
         // Acceptable tax amount differences
-        $details->taxAmount = '2.35';
-        $details->surchargeAmount = '0.57';
+        $details->taxAmount = '1.11';
         $details->validate();
-        $details->taxAmount = '2.37';
-        $details->surchargeAmount = '0.59';
+        $details->taxAmount = '1.13';
         $details->validate();
     }
 
-    public function testValidatesOperationType(): void {
+    public function testValidatesSurchargeAmount(): void {
+        $details = new BreakdownDetails();
+        $details->taxType = TaxType::IVA;
+        $details->regimeType = RegimeType::C18;
+        $details->operationType = OperationType::Subject;
+        $details->baseAmount = '33.22';
+        $details->taxRate = '21.00';
+        $details->taxAmount = '6.98';
+        $details->surchargeRate = '5.20';
+        $details->surchargeAmount = '1.73';
+
+        // Should pass validation
+        $details->validate();
+
+        // Wrong tax amount
+        $details->taxAmount = '99.99';
+        $details->surchargeAmount = '12.34';
+        try {
+            $details->validate();
+            $this->fail('Did not throw exception for invalid tax amount');
+        } catch (InvalidModelException $e) {
+            $this->assertStringContainsString('Expected tax amount of 6.98, got 99.99', $e->getMessage());
+            $this->assertStringContainsString('Expected surcharge amount of 1.73, got 12.34', $e->getMessage());
+        }
+
+        // Acceptable tax amount differences
+        $details->taxAmount = '6.97';
+        $details->surchargeAmount = '1.74';
+        $details->validate();
+        $details->taxAmount = '6.99';
+        $details->surchargeAmount = '1.73';
+        $details->validate();
+    }
+
+    public function testValidatesOperationAndRegimeType(): void {
         $details = new BreakdownDetails();
         $details->taxType = TaxType::IVA;
         $details->baseAmount = '100.00';
 
-        // Check error message when tax is not given and is subject
-        $details->regimeType = RegimeType::C18;
+        // Missing tax details for subject operations
+        $details->regimeType = RegimeType::C01;
         $details->operationType = OperationType::Subject;
         try {
             $details->validate();
@@ -57,47 +85,48 @@ final class BreakdownDetailsTest extends TestCase {
         } catch (InvalidModelException $e) {
             $this->assertStringContainsString('Tax rate must be defined for subject operation types', $e->getMessage());
             $this->assertStringContainsString('Tax amount must be defined for subject operation types', $e->getMessage());
+        }
+
+        // Missing surcharge details for C18 regimes
+        $details->regimeType = RegimeType::C18;
+        $details->taxRate = '21.00';
+        $details->taxAmount = '21.00';
+        try {
+            $details->validate();
+            $this->fail('Did not throw exception for missing surcharge rate and amount');
+        } catch (InvalidModelException $e) {
             $this->assertStringContainsString('Surcharge rate must be defined for C18 regime type', $e->getMessage());
             $this->assertStringContainsString('Surcharge amount must be defined for C18 regime type', $e->getMessage());
         }
 
-        // Check no error message when tax is not given and is exempt
+        // Extra surcharge details for C01 regimes
         $details->regimeType = RegimeType::C01;
-        $details->operationType = OperationType::ExemptByOther;
-        $details->validate();
-
-        $details->taxRate = '21.00';
-        $details->taxAmount = '21.00';
         $details->surchargeRate = '5.20';
         $details->surchargeAmount = '5.20';
-
-        // Check error message when tax is given and is exempt
-        $details->regimeType = RegimeType::C01;
-        $details->operationType = OperationType::ExemptByOther;
         try {
             $details->validate();
-            $this->fail('Did not throw exception for defined tax rate and ammount when the operation is exempt');
-        } catch (InvalidModelException $e) {
-            $this->assertStringContainsString('Tax rate cannot be defined for non-subject or exempt operation types', $e->getMessage());
-            $this->assertStringContainsString('Tax amount cannot be defined for non-subject or exempt operation types', $e->getMessage());
-            $this->assertStringContainsString('Surcharge rate cannot be defined for non-subject or exempt operation types', $e->getMessage());
-            $this->assertStringContainsString('Surcharge amount cannot be defined for non-subject or exempt operation types', $e->getMessage());
-        }
-
-        // Check error when surcharge rate and ammount are given for subject
-        $details->regimeType = RegimeType::C01;
-        $details->operationType = OperationType::Subject;
-        try {
-            $details->validate();
-            $this->fail('Did not throw exception for defined surcharge rate and ammount when the operation is not C18 regime type');
+            $this->fail('Did not throw exception for extra surcharge rate and amount');
         } catch (InvalidModelException $e) {
             $this->assertStringContainsString('Surcharge rate cannot be defined for non-C18 regime types', $e->getMessage());
             $this->assertStringContainsString('Surcharge amount cannot be defined for non-C18 regime types', $e->getMessage());
         }
 
-        // Check no error message when tax is given and is subject
-        $details->regimeType = RegimeType::C18;
-        $details->operationType = OperationType::Subject;
+        // Extra tax details for exempt operations
+        $details->operationType = OperationType::ExemptByArticle20;
+        try {
+            $details->validate();
+            $this->fail('Did not throw exception for extra tax rate and amount');
+        } catch (InvalidModelException $e) {
+            $this->assertStringContainsString('Tax rate cannot be defined for non-subject or exempt operation types', $e->getMessage());
+            $this->assertStringContainsString('Tax amount cannot be defined for non-subject or exempt operation types', $e->getMessage());
+        }
+
+        // Should pass validation for exempt operations
+        $details->taxRate = null;
+        $details->taxAmount = null;
+        $details->surchargeRate = null;
+        $details->surchargeAmount = null;
+        $details->operationType = OperationType::ExemptByOther;
         $details->validate();
     }
 }
